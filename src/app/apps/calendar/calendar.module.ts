d25d9b6e3dc6e7ad6a4ebeb72e9e6f210beae425
aldrichclementd <lawsonaldrich99@gmail.com>
2023-10-16 18:24:55 +0200

    first commit

---
+++ src/app/apps/calendar/calendar.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
+import { PageTitleModule } from 'src/app/shared/page-title/page-title.module';
+import { WelcomeCardComponent } from './welcome-card/welcome-card.component';
+import { CalendarEventComponent } from './event/event.component';
+import { CalendarRoutingModule } from './calendar-routing.module';
+import { CalendarComponent } from './calendar.component';
+
+
+
+// FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+//   dayGridPlugin,
+//   interactionPlugin,
+//   bootstrapPlugin,
+//   timeGridPlugin,
+//   listPlugin
+// ]);
+
+@NgModule({
+  declarations: [
+    CalendarComponent,
+    WelcomeCardComponent,
+    CalendarEventComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FullCalendarModule,
+    PageTitleModule,
+    CalendarRoutingModule
+  ]
+})
+export class CalendarModule { }
---
+++ src/app/shared/ui/feather-icon.directive.ts
@@ -0,0 +1,15 @@
+import { Directive } from '@angular/core';
+// import feather from 'feather-icons';
+@Directive({
+  selector: '[appFeatherIcon]'
+})
+export class FeatherIconDirective {
+
+  constructor () { }
+
+  ngAfterViewInit() {
+    // feather icon
+    // feather.replace();
+  }
+
+}
